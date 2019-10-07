a442855741ceca7d69ead042cbd887c5e9d06719
Tobias Wittwer <t.wittwer95@gmail.com>
2019-10-07 22:43:58 +0200

    Enable local setup

--- main.ts
+++ main.ts
@@ -11,10 +11,3 @@ platformBrowserDynamic()
     window['ngRef'] = ref;
   })
   .catch(err => console.error(err));
-
-/**
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
